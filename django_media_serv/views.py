912d5ce170420b3a1cde8da68bf654370e918216
Jason Moore <jmoore@austwa.com>
2025-11-03 11:46:40 +0800

    Update views.py

--- django_media_serv/views.py
+++ django_media_serv/views.py
@@ -1,7 +1,17 @@
-from django.http import Http404, HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, JsonResponse, HttpResponseRedirect, StreamingHttpResponse
 from django.conf import settings
 import mimetypes
 import os
+
+
+
+def file_iterator(file_name, chunk_size=8192):
+    with open(file_name, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
 
 def getMediaFile(request):
     file_path = request.path.replace("../","")
@@ -15,28 +25,28 @@ def getMediaFile(request):
 
     file_name_path = settings.BASE_DIR+file_path
     if os.path.isfile(file_name_path) is True:
-              the_file = open(file_name_path, 'rb')
-              the_data = the_file.read()
-              the_file.close()
+              # the_file = open(file_name_path, 'rb')
+              # the_data = the_file.read()
+              # the_file.close()
               if extension == 'msg':
-                  return HttpResponse(the_data, content_type="application/vnd.ms-outlook")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="application/vnd.ms-outlook")
               if extension == 'eml':
-                  return HttpResponse(the_data, content_type="application/vnd.ms-outlook")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="application/vnd.ms-outlook")
               if extension == 'shp':
-                  return HttpResponse(the_data, content_type="x-gis/x-shapefile")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="x-gis/x-shapefile")
               if extension == 'shx':
-                  return HttpResponse(the_data, content_type="x-gis/x-shapefile")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="x-gis/x-shapefile")
               if extension == 'dbf':
-                  return HttpResponse(the_data, content_type="text/plain")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="text/plain")
               if extension == 'prj':
-                  return HttpResponse(the_data, content_type="application/octet-stream")
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="application/octet-stream")
               if extension == 'gpx':
-                  return HttpResponse(the_data, content_type="application/gpx+xml")            
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="application/gpx+xml")            
               if extension == 'docx':
-                  return HttpResponse(the_data, content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")                
+                  return StreamingHttpResponse(file_iterator(file_name_path), content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")                
 
               if len(extension) > 2:
-                 return HttpResponse(the_data, content_type=mimetypes.types_map['.'+str(extension.lower())])
+                 return StreamingHttpResponse(file_iterator(file_name_path), content_type=mimetypes.types_map['.'+str(extension.lower())])
               else:
                  return HttpResponse("ERROR opening file", content_type="text/plain")
     else:
